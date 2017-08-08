a86579f3fec34073bf7b1cf941716b246f84f3c9
Abhishek Bansal <discover.ab@gmail.com>
2017-08-08 17:51:19 +0530

    + adding python script for bootstrapping android project. This should deprecated old yeoman based script.

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
-# JumpstartScript
-### Please clone this repository at the desired location.
-### Put the file in the directory where you want to create new project.
-### Run ./JumpstartScript.sh ProjectDirectoryName and your project is created.
-### If you need to create template for jumpstart again just go to directory and just JumpstartScript ProjectDirectoryName and Bang You get your new template. No need to add Copy JumpstartScript everytime in each folder. We took care of it for You.
+# Jumpstart Python Script
+1. Copy this script at desired location.
+2. execute `python android-jumpstart.py`
+3. Follow on screen instructions and you should be good.
+
+# Jumpstart Shell Script (Now Deprecated)
+Please clone this repository at the desired location.
+Put the file in the directory where you want to create new project.
+Run `./JumpstartScript.sh ProjectDirectoryName` and your project is created.
+If you need to create template for jumpstart again just go to directory and just JumpstartScript ProjectDirectoryName and Bang You get your new template. No need to add Copy JumpstartScript everytime in each folder. We took care of it for You.
 
 # Happy Coding...
---
+++ android-jumpstart.py
@@ -0,0 +1,102 @@
+import os
+import shutil
+import distutils
+from subprocess import call
+from distutils.dir_util import copy_tree
+
+# Dev Notes
+# Only use built in python libraries to keep configuration to minimum
+
+# TODO
+# 1. Refactor this code into classes
+# 2. Auto push to newly created repo
+# 3. build.gradle processor which allows to add/remove libraries on the fly
+#    and other stuff
+
+package=""
+projectName=""
+
+# Jumpstart Project Configuration
+jumpstartPackage = "com.moldedbits.android"
+jumpstartDirectoryName = "android-jumpstart"
+jumpstartRepoURL = "https://github.com/moldedbits/android-jumpstart.git"
+srcRelativeRoot = "/app/src/<srcSet>/java/"
+
+def cloneJumpstartRepo():
+    call(["git", "clone", jumpstartRepoURL])
+
+def createNewDirectories():
+    global projectName
+    while (not projectName):
+        projectName = raw_input("Enter new app name: ")
+        if(projectName == ""):
+            print("Project name cannot be empty")
+
+    # TODO tobe removed
+    if(os.path.exists(projectName)):
+        shutil.rmtree(projectName)
+
+    print("Copying stuff from " + jumpstartDirectoryName + " to " + projectName)
+    shutil.copytree(jumpstartDirectoryName, projectName)
+    print("Done Copying")
+
+def changeSourcePackage(srcSet):
+    relativeRoot = srcRelativeRoot.replace("<srcSet>", srcSet)
+    print (srcSet + ": " + relativeRoot)
+
+    srcPath = projectName +  relativeRoot + jumpstartPackage.replace(".", "/")
+    if(os.path.exists(srcPath)):
+        print("creating directory structure for new package: " + package)
+        packagePath = package.replace(".", "/")
+        currentPath = projectName + relativeRoot + "temp/" + packagePath
+        print("Copying stuff from " + srcPath + " to " + currentPath)
+        shutil.copytree(srcPath, currentPath)
+        print("Copying Done! Now removing old stuff")
+        shutil.rmtree(projectName + relativeRoot + "com")
+        print("Deleting Done!")
+        print("Moving new things where they belong!")
+
+        # copying things in a temp folder first in order to avoid same name clashes
+        print projectName + relativeRoot + "temp/" + " to " + projectName + relativeRoot
+        copy_tree(projectName + relativeRoot + "temp/", projectName + relativeRoot)
+        print("Removing temporary directory")
+        shutil.rmtree(projectName + relativeRoot + "temp")
+    else:
+        print "Path does not exist " + srcPath
+
+
+def replacePackageInSource():
+    # traverse root directory, and list directories as dirs and files as files
+    for root, dirs, files in os.walk("./" + projectName):
+        for file in files:
+            filePath = root + "/" + file
+            if(".git/" in filePath):
+                print "Skipping: " + filePath
+                continue
+            else:
+                print(filePath)
+
+            fileText = ""
+            with open(filePath) as f:
+                fileText = f.read().replace(jumpstartPackage, package)
+
+            with open(filePath, "w") as f:
+                f.write(fileText)
+
+cloneJumpstartRepo()
+createNewDirectories()
+
+while (not package):
+    package = raw_input("Enter new app package: ")
+    if(package == ""):
+        print("Package name cannot be empty")
+
+# TODO this can be improved. Iterate the directory instead
+changeSourcePackage("main")
+changeSourcePackage("androidTest")
+changeSourcePackage("debug")
+changeSourcePackage("debugProd")
+changeSourcePackage("release")
+changeSourcePackage("test")
+
+replacePackageInSource()
